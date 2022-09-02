80a1aae42753cc5eaccff8946e9be89a65b5e486
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-09-02 03:24:58 +0000

    Added mocha chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,21 @@
 const request = require('request');
 
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback('URL error', null);
+    }
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    // console.log(typeof body);
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback('ERROR: Breed not found', 'N/A' );
+    } else {
+      callback(null, data[0]['description']);
+    }
+  });
+};
 
-const breed = process.argv.splice(2);
-
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('URL error');
-  }
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  // console.log(typeof body);
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log('ERROR: Breed not found');
-  } else {
-    console.log(data[0]['description']);
-  }
-});
-
-
+module.exports = { fetchBreedDescription };
